8b2a0b33171d2d5196ab0ac2ea74ab9cd853eb6a
konpure <104032111@qq.com>
2025-08-22 15:58:34 +0800

    feat: add ResourceManager to poll CPU & memory usage

--- go.mod
+++ go.mod
@@ -5,12 +5,15 @@ go 1.24.0
 require (
 	github.com/cilium/ebpf v0.19.0
 	github.com/quic-go/quic-go v0.54.0
+	github.com/shirou/gopsutil/v3 v3.20.10
 	google.golang.org/grpc v1.75.0
 	google.golang.org/protobuf v1.36.8
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
 	go.uber.org/mock v0.6.0 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/mod v0.27.0 // indirect
--- internal/edge-agent/resource_manager.go
+++ internal/edge-agent/resource_manager.go
@@ -1,1 +1,75 @@
 package edge_agent
+
+import (
+	"context"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
+	"log/slog"
+	"time"
+)
+
+type ResourceManager struct {
+	ctx       context.Context
+	cancel    context.CancelFunc
+	interval  time.Duration
+	threshold float64
+}
+
+func NewResourceManager(interval time.Duration, threshold float64) *ResourceManager {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &ResourceManager{
+		ctx:       ctx,
+		cancel:    cancel,
+		interval:  interval,
+		threshold: threshold,
+	}
+}
+
+func (rm *ResourceManager) Start() {
+	slog.Info("Starting resource manager", "interval", rm.interval, "threshold", rm.threshold)
+
+	go func() {
+		ticker := time.NewTicker(rm.interval)
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				if err := rm.checkResource(); err != nil {
+					slog.Error("Failed to check resource", "error", err)
+				}
+			case <-rm.ctx.Done():
+				slog.Info("Resource manager stopped")
+				return
+			}
+		}
+	}()
+}
+
+func (rm *ResourceManager) checkResource() error {
+	// TODO: Implement resource checking logic here
+	cpuUsage, err := cpu.Percent(0, false)
+	if err != nil {
+		return err
+	}
+
+	memInfo, err := mem.VirtualMemory()
+	if err != nil {
+		return err
+	}
+
+	slog.Debug("Resourece usage",
+		"cpu_usage", cpuUsage[0],
+		"mem_usage", memInfo.UsedPercent,
+	)
+
+	if cpuUsage[0] > rm.threshold {
+		slog.Warn("High CPU usage detected", "cpu_usage", cpuUsage[0], "threshold", rm.threshold)
+	}
+
+	if memInfo.UsedPercent > rm.threshold {
+		slog.Warn("High memory usage detected", "mem_usage", memInfo.UsedPercent, "threshold", rm.threshold)
+	}
+
+	return nil
+}
